af76dfd616f2861f3a4ebf9fc4f884d8e6f5d5e3
gamba <ano.gambashidze@gmail.com>
2024-08-26 00:33:27 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import  "./index.css"
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <div className="message">hello</div>
+      <div className="buttons">
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
     </div>
   );
 }
